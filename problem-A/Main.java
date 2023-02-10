5be0197ab9df9bac2e5ada9e0e223edc374f30b3
ChakChingis <118927732+ChakChingis@users.noreply.github.com>
2023-02-10 22:51:41 +0600

    Update Main.java

--- problem-A/Main.java
+++ problem-A/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args){
-        Dog a = new Dog("Barsik");
-        System.out.println(a);
         Animal x = new Animal("Elephant");
         Mammal q = new Mammal("Elephant");
         Cat e = new Cat("Mursik");
+        Dog a = new Dog("Barsik");
         System.out.println(x);
         System.out.println(q);
         System.out.println(e);
+        System.out.println(a);
         e.greets();
         a.greets();
     }
