4a30e5c3af02a783428789ab1d0e9f7a75a3fa55
ChakChingis <118927732+ChakChingis@users.noreply.github.com>
2023-02-11 00:42:06 +0600

    Update Main.java

--- problem-A/Main.java
+++ problem-A/Main.java
@@ -1,3 +1,48 @@
+public class Animal {
+     String name;
+     public Animal(String name){
+         this.name = name;
+     }
+     public  String toString(){
+         return "Animal[name ="  + name+"]";
+     }
+
+ }
+ class Mammal extends Animal {
+     public Mammal(String name) {
+         super(name);
+     }
+
+     public String toString() {
+         return "Mammal[" + super.toString() + "]";
+     }
+ }
+ class Cat extends Mammal{
+    public Cat(String name){
+        super(name);
+    }
+    public static void greets(){
+        System.out.println("Meow");
+    }
+    public String toString(){
+        return "Cat["+ super.toString()+"]";
+    }
+ }
+ class Dog extends Cat{
+    public Dog(String name){
+        super(name);
+    }
+    public static void greets(){
+        System.out.println("Woof");
+    }
+    public static void greets(Dog another){
+        System.out.println("Woooof");
+    }
+
+    public String toString(){
+        return "Dog["+super.toString()+"]";
+    }
+ }
 public class Main {
     public static void main(String[] args){
         Animal x = new Animal("Elephant");
